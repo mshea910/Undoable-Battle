f7d7284aa0ed1854f41f0ed5dda7d18db9cdc1bc
mshea910 <mshea6@unh.newhaven.edu>
2022-09-01 17:34:06 -0400

    Begin main program runner; Add enum class and refactor

---
+++ GameManagement/Enums.h
@@ -0,0 +1,11 @@
+//
+// Created by Michael Shea on 9/1/22.
+//
+
+#ifndef PROGRAM3_MICHAELSHEA_ENUMS_H
+#define PROGRAM3_MICHAELSHEA_ENUMS_H
+
+enum MoveType{AttackOne, AttackTwo, Heal};
+enum Enums{Ghost, Warrior, Knight};
+
+#endif //PROGRAM3_MICHAELSHEA_ENUMS_H
